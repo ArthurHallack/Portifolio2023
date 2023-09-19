c51e970d18a6befde217b41a114326a62cff4823
Arthur <ArthurHallack2@gmail.com>
2023-09-19 00:24:42 -0300

    sand-box

--- src/components/Projetos.jsx
+++ src/components/Projetos.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import todolistImg from '../img/todolist.png'
 
-const estilo1={
-    width: '140px'
-}
 
 function Projetos () {
     return (
@@ -12,38 +9,14 @@ function Projetos () {
             <ul id='listaProjetos'>
                 <li>
                     <div className='Projetos'>
-                        <img src={todolistImg} alt="tdlist"/>
+                        <img src={todolistImg} alt="tdlist" className='imgProjetos'/>
                         <h2>To-Do-List</h2>
                         <p>Uma lista de tarefas com tema de uma obra chamada Jujutsu Kaisen. Desenvolvida com o propósito de exercitar a implementação das funcionalidades de: Pesquisar, Filtrar, Adicionar, Editar e Deletar. </p>
                         <p>Tecnologias utilizadas: Html5, Css3, Javascript.</p>
-                        <button style={estilo1}>Visualizar</button>
-                    </div>
-                </li>
-                <li>
-                    <div className='Projetos'>
-                        <img src="" alt="" />
-                        <h2>Cronometro.js</h2>
-                        <p></p>
-                        <p></p>
-                        <button></button>
-                    </div>
-                </li>
-                <li>
-                    <div className='Projetos'>
-                        <img src="" alt="" />
-                        <h2>Amigo Secreto PHP</h2>
-                        <p></p>
-                        <p></p>
-                        <button></button>
-                    </div>
-                </li>
-                <li>
-                    <div className='Projetos'>
-                        <img src="" alt="" />
-                        <h2>Em breve...</h2>
-                        <p></p>
-                        <p></p>
-                        <button></button>
+                        <div className='BTNprojetos-container'>
+                        <button className='BTNprojetos'>Visualizar</button>
+                        </div>
+                        
                     </div>
                 </li>
             </ul>
