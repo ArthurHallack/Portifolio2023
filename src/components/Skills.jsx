f581b8575b2d51c540fc32214c2013b57e9c5672
Arthur <ArthurHallack2@gmail.com>
2023-12-07 22:59:09 -0300

    sand-box

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import htmlImg from '../img/html.png'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
 import cssImg from '../img/css.png'
 import reactImg from '../img/react.png'
 import nodeImg from '../img/node.png'
@@ -21,18 +23,54 @@ function Skills () {
         <div id='slide-container'>
             <div className='slide-content'>
                 <div className='card-wrapper'>
-                    <div className='card'>
-                        <div className='image-content'>
-                            <span className='over-lay'></span>
-                            <div className='card-image'>
-                                <img src={htmlImg} alt="html" className='card-img' />
+                    <Swiper spaceBetween={0}
+                            slidesPerView={3}
+                            onSlideChange={() => console.log('slide change')}
+                            onSwiper={(swiper) => console.log(swiper)}>
+                        <SwiperSlide>
+                            <div className='card'>
+                                <div className='image-content'>
+                                    <span className='over-lay'></span>
+                                    <div className='card-image'>
+                                        <img src={htmlImg} alt="html" className='card-img' />
+                                    </div>
+                                </div>
+                                <div className='card-content'>
+                                    <h2 className='name'>HTML5</h2>
+                                    <p className='descricao'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur cumque laborum harum voluptate quaerat placeat magnam, aut at aliquam tempora autem vel maiores nulla minus quasi odit modi ratione dicta!</p>
+                                </div>
                             </div>
-                        </div>
-                        <div className='card-content'>
-                            <h2 className='name'>HTML5</h2>
-                            <p className='descricao'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur cumque laborum harum voluptate quaerat placeat magnam, aut at aliquam tempora autem vel maiores nulla minus quasi odit modi ratione dicta!</p>
-                        </div>
-                    </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className='card'>
+                                <div className='image-content'>
+                                    <span className='over-lay'></span>
+                                    <div className='card-image'>
+                                        <img src={htmlImg} alt="html" className='card-img' />
+                                    </div>
+                                </div>
+                                <div className='card-content'>
+                                    <h2 className='name'>HTML5</h2>
+                                    <p className='descricao'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur cumque laborum harum voluptate quaerat placeat magnam, aut at aliquam tempora autem vel maiores nulla minus quasi odit modi ratione dicta!</p>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className='card'>
+                                <div className='image-content'>
+                                    <span className='over-lay'></span>
+                                    <div className='card-image'>
+                                        <img src={htmlImg} alt="html" className='card-img' />
+                                    </div>
+                                </div>
+                                <div className='card-content'>
+                                    <h2 className='name'>HTML5</h2>
+                                    <p className='descricao'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur cumque laborum harum voluptate quaerat placeat magnam, aut at aliquam tempora autem vel maiores nulla minus quasi odit modi ratione dicta!</p>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                        ...
+                    </Swiper>
                 </div>
             </div>
         </div>
